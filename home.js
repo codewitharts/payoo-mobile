ec8c7f1d65271683680768f38f3e4cec0ba807f8
Ashikurahman23 <sm.ashikurahman@gmail.com>
2025-08-26 04:15:24 +0600

    add money

--- home.js
+++ home.js
@@ -2,13 +2,16 @@
 function myElement(id) {
   return document.getElementById(id);
 }
-
+const initialNumber = '01811223344';
+const initialPin = '1234';
 // Logout Functionality
 myElement('logout-btn').addEventListener('click', function () {
   window.location.href = './index.html';
 });
 
 // Display Balance
+let displayAmount = myElement('display-amount').innerText;
+let convertedAmount = parseInt(displayAmount);
 // Add Money
 const addMoney = myElement('add-money-btn');
 addMoney.addEventListener('click', function (e) {
@@ -17,19 +20,31 @@ addMoney.addEventListener('click', function (e) {
   const accountNumber = addMoney.parentNode.children[3].value;
   const addAmount = addMoney.parentNode.children[5].value;
   const pinNumber = addMoney.parentNode.children[7].value;
+
+  const convertAmount = parseFloat(addAmount);
+
   if (bankName === 'Select Bank') {
     alert('Please select a Bank');
     return;
   }
-
-  if (isNaN(accountNumber) || isNaN(addAmount) || isNaN(pinNumber)) {
-    alert('Please give correct Number & Pin');
+  if (
+    accountNumber.length !== 11 || // Length must be 11
+    isNaN(accountNumber) || // Must be a number
+    accountNumber.includes('.') || // Must not be a decimal
+    accountNumber.trim() === '' // Must not be empty
+  ) {
+    alert('Account Number must be exactly 11 digits and numeric');
+    return;
+  }
+  if (isNaN(convertAmount) || convertAmount <= 0) {
+    alert('Enter valid amount');
+    return;
+  }
+  if (pinNumber !== initialPin) {
+    alert('Invalid Pin');
+    return;
   }
 
-  let displayAmount = myElement('display-amount').innerText;
-  let convertedAmount = parseInt(displayAmount);
-  convertedAmount = Number(convertedAmount) + Number(addAmount);
+  convertedAmount = parseFloat(convertedAmount) + parseFloat(addAmount);
   myElement('display-amount').innerText = convertedAmount;
-
-  addMoney.classList.remove('hidden');
 });
